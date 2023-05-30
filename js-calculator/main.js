5d914d3ba0842f673a28e2e1762579c76b3f55b4
Leandro_SmS <leandrosimoesms@gmail.com>
2023-05-30 06:30:07 -0300

    Set styles, adjust colors and divs

--- js-calculator/main.js
+++ js-calculator/main.js
@@ -2,12 +2,10 @@ const { createRoot } = ReactDOM;
 
 const Calculator = () => {
     return (
-        <div className="calculator">
+        <div id="calculator">
             <div id="display"></div>
+            <div id="clear">AC</div>
             <div id="numbers">
-                <div className="number" id="decimal">.</div>
-                <div className="number" id="zeroZero">00</div>
-                <div className="number" id="zero">0</div>
                 <div className="number" id="one">1</div>
                 <div className="number" id="two">2</div>
                 <div className="number" id="three">3</div>
@@ -17,6 +15,9 @@ const Calculator = () => {
                 <div className="number" id="seven">7</div>
                 <div className="number" id="eigth">8</div>
                 <div className="number" id="nine">9</div>
+                <div className="number" id="zero">0</div>
+                <div className="number" id="zeroZero">00</div>
+                <div className="number" id="decimal">.</div>
             </div>
             <div id="operators">
                 <div className="operator" id="add">+</div>
@@ -24,6 +25,7 @@ const Calculator = () => {
                 <div className="operator" id="divide">/</div>
                 <div className="operator" id="multiply">*</div>
             </div>
+            <div id="equals">=</div>
         </div>
     )
 };
