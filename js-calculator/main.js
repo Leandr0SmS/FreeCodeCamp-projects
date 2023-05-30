12475e4181f710309705f583645c8b3f7c416528
Leandro_SmS <leandrosimoesms@gmail.com>
2023-05-30 06:47:54 -0300

    Set grid flex and gap

--- js-calculator/main.js
+++ js-calculator/main.js
@@ -3,29 +3,31 @@ const { createRoot } = ReactDOM;
 const Calculator = () => {
     return (
         <div id="calculator">
-            <div id="display"></div>
-            <div id="clear">AC</div>
-            <div id="numbers">
-                <div className="number" id="one">1</div>
-                <div className="number" id="two">2</div>
-                <div className="number" id="three">3</div>
-                <div className="number" id="four">4</div>
-                <div className="number" id="five">5</div>
-                <div className="number" id="six">6</div>
-                <div className="number" id="seven">7</div>
-                <div className="number" id="eigth">8</div>
-                <div className="number" id="nine">9</div>
-                <div className="number" id="zero">0</div>
-                <div className="number" id="zeroZero">00</div>
-                <div className="number" id="decimal">.</div>
+            <div className="btn" id="display"></div>
+            <div className="btn" id="clear">AC</div>
+            <div id="main">
+                <div id="numbers">
+                    <div className="number btn" id="one">1</div>
+                    <div className="number btn" id="two">2</div>
+                    <div className="number btn" id="three">3</div>
+                    <div className="number btn" id="four">4</div>
+                    <div className="number btn" id="five">5</div>
+                    <div className="number btn" id="six">6</div>
+                    <div className="number btn" id="seven">7</div>
+                    <div className="number btn" id="eigth">8</div>
+                    <div className="number btn" id="nine">9</div>
+                    <div className="number btn" id="zero">0</div>
+                    <div className="number btn" id="zeroZero">00</div>
+                    <div className="number btn" id="decimal">.</div>
+                </div>
+                <div id="operators">
+                    <div className="operator btn" id="add">+</div>
+                    <div className="operator btn" id="subtract">-</div>
+                    <div className="operator btn" id="divide">/</div>
+                    <div className="operator btn" id="multiply">*</div>
+                </div>
             </div>
-            <div id="operators">
-                <div className="operator" id="add">+</div>
-                <div className="operator" id="subtract">-</div>
-                <div className="operator" id="divide">/</div>
-                <div className="operator" id="multiply">*</div>
-            </div>
-            <div id="equals">=</div>
+            <div className="btn" id="equals">=</div>
         </div>
     )
 };
