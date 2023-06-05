0a58e679328edf2ccb0773e17afb6d907068fbeb
Leandro_SmS <leandrosimoesms@gmail.com>
2023-06-05 06:24:18 -0300

    Set icons, divide controls and timer into diferents components

--- clock/main.js
+++ clock/main.js
@@ -1,12 +1,36 @@
 const { createRoot } = ReactDOM;
 
+const Selectors = ({ id, label, selected }) => {
+    return (
+        <div className="selectors">
+            <h1 id={id}>{label}</h1>
+            <div className="controls--div">
+                <div className="controls">
+                    <span className="material-symbols-outlined arrow">expand_less</span>
+                    <span className="material-symbols-outlined arrow">expand_more</span>
+                </div>
+                <h2 className="number">{selected}</h2>
+            </div>
+        </div>
+    )
+};
+
+const Timer = ({ number }) => {
+    return (
+        <div className="timer--div">
+            <div className="timer">{number}</div>
+            <span className="material-symbols-outlined" id="play_pause">play_pause</span>
+            <span className="material-symbols-outlined">refresh</span>
+        </div>
+    )
+};
+
 const App = () => {
     return (
-        <div>
-            <h1>Hey</h1>
-            <p>12345678900-</p>
-        </div>
-        
+        <React.Fragment>
+            <Selectors id="break-label" label="Break Length" selected={5}/>
+            <Selectors id="session-label" label="Session Length" selected={25}/>
+        </React.Fragment>
     )
 };
 
