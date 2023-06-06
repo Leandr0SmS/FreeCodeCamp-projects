d1e05bc0c3f25ae59ef4fa213d73c4f8e240ad21
Leandro_SmS <leandrosimoesms@gmail.com>
2023-06-06 05:35:04 -0300

    Set time to have two digits

---
+++ clock/functions/functions.js
@@ -0,0 +1,20 @@
+const counter = (min, sec) => {
+    let minLeft = min;
+    let secLeft = sec;
+    if (minLeft < 0 ||  secLeft < 0) {
+        return console.log("Error - negative time")
+    }
+    if (minLeft <= 60 && secLeft < 60) {
+        if (secLeft > 0) {
+            return [minLeft, (secLeft - 1)]
+        } else if (minLeft > 0 && secLeft == 0) {
+            return [(minLeft - 1), 59]
+        } else if (minLeft == 0 && secLeft == 0) {
+            return [0, 0]
+        }
+    } else {
+        return console.log("Error - invalid time to count")
+    }
+};
+
+export { counter }
