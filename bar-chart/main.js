8c895b9d5b90fb531a7eb5c6e5e7f458aff3b7b2
Leandro_SmS <leandrosimoesms@gmail.com>
2023-06-08 06:28:42 -0300

    fetch data and start to set bars

--- bar-chart/main.js
+++ bar-chart/main.js
@@ -20,6 +20,17 @@ const App = () => {
                       .append('svg')
                       .attr('width', w)
                       .attr('height', h)
+
+        svg.selectAll('rect')
+           .data(data.data)
+           .enter()
+           .append('rect')
+           .attr('x', (d, i) => i * 30)
+           .attr('y', (d, i) => h - 3 * d)
+           .attr('width', 25)
+           .attr('height', (d, i) => 3 * d)
+           .fill('fill', 'navy')
+
     }, []);
 
     return (
