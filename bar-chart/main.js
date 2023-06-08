34ce9b222dec6fb302d4cc9e426375ca5322d963
Leandro_SmS <leandrosimoesms@gmail.com>
2023-06-08 06:28:12 -0300

    fetch data

--- bar-chart/main.js
+++ bar-chart/main.js
@@ -1,8 +1,31 @@
 const { createRoot } = ReactDOM;
+const { useState, useEffect } = React;
 
 const App = () => {
+
+    const [data, setData] = useState("");
+
+    const w = 500;
+    const h = 500;
+
+    useEffect(() => {
+
+        fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")
+            .then(res => res.json())
+            .then(data => {
+            console.log(data)
+            setData(data)
+    })
+        const svg = d3.select('#app')
+                      .append('svg')
+                      .attr('width', w)
+                      .attr('height', h)
+    }, []);
+
     return (
-        <h1>Hey</h1>
+        <div id='app'>
+            <h1 id="title">United States GDP</h1>
+        </div>
     )
 }
 
