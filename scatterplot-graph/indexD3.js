1781e1f249f660a2f02b27bf1ce99c443d991b43
Leandro_SmS <leandrosimoesms@gmail.com>
2023-06-14 14:39:08 -0300

    Set tooltip

--- scatterplot-graph/indexD3.js
+++ scatterplot-graph/indexD3.js
@@ -7,9 +7,14 @@ export const renderD3 = (data, width, height) => {
     const padding = 40;
 
     const svg = d3.select('#container')
-    .append('svg')
-    .attr('width', w)
-    .attr('height', h);
+        .append('svg')
+        .attr('width', w)
+        .attr('height', h)
+        .attr('class', 'svg');
+
+    const tooltip = d3.select('#container')
+        .append('div')
+        .attr('id', 'tooltip');
 
     const countries =  [...new Set(data.map(d => d.Nationality))];
 
@@ -21,22 +26,22 @@ export const renderD3 = (data, width, height) => {
     svg.selectAll("text")
         .data(countries)
         .enter()
-        .append("text")
-        .attr("x", w / 5 * 4)
-        .attr("y", (d, i) => padding + (i * 25))
+        .append('text')
+        .attr('x', w / 5 * 4)
+        .attr('y', (d, i) => padding + (i * 25))
         .text(d =>`${d}`)
-        .attr("class","legend")
+        .attr('class','legend')
 
-    svg.selectAll("rect")
+    svg.selectAll('rect')
         .data(countries)
         .enter()
-        .append("rect")
-        .attr("x", (w / 5 * 4) - 20)
-        .attr("y", (d, i) => (padding + (i * 25)) - 13)
+        .append('rect')
+        .attr('x', (w / 5 * 4) - 20)
+        .attr('y', (d, i) => (padding + (i * 25)) - 13)
         .attr('width', '1rem')
         .attr('height', '1rem')
         .text(d =>`${d}`)
-        .attr("class","legend-color")
+        .attr('class', 'legend-color')
         .style('fill', (d, i) => {
             if (colorsMap.has(d)) {
                 return colorsMap.get(d)
@@ -44,10 +49,10 @@ export const renderD3 = (data, width, height) => {
         })
 
     svg.append('text')
-        .attr("x", (w / 5 * 4) - 20)
-        .attr("y", padding - 20)
-        .text("LEGENDS:")
-        .attr("id", "legend")
+        .attr('x', (w / 5 * 4) - 20)
+        .attr('y', padding - 20)
+        .text('LEGENDS:')
+        .attr('id', 'legend')
 
     const years = data.map(d => {
         return d.Year = new Date(d.Year, 0, 1); 
@@ -112,6 +117,30 @@ export const renderD3 = (data, width, height) => {
             return times[i]
         })
         .attr('fill', (d) => colorsMap.get(d.Nationality))
+        .on('mouseover', (event, i) => {
+            tooltip
+                .attr('data-year', years[i].getFullYear())
+                .transition()
+                .duration(20)
+                .style('opacity', 0.9);
+            tooltip
+                .html(
+                  data[i].Name +
+                    ': ' +
+                    data[i].Nationality +
+                    '<br/>' +
+                    'Year: ' +
+                    data[i].Year +
+                    (data[i].Doping ? '<br/><br/>' + data[i].Doping : '')
+                )
+                .attr('data-date', data[i][0])
+        })
+        .on('mouseout', () => {
+            tooltip
+                .transition()
+                .duration(200)
+                .style('opacity', 0);
+        });
 
         console.log(colorsMap)
         console.log(data)
