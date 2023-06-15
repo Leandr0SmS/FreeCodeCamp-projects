8e8e738a9617b731716cf49fc984d922509a41f6
Leandro_SmS <leandrosimoesms@gmail.com>
2023-06-15 06:42:23 -0300

    Set functions folder, added months.js to select proper month string, set axis to graph

--- heat-map/indexD3.js
+++ heat-map/indexD3.js
@@ -1,20 +1,23 @@
+import { monthsSwitch } from "./functions/months.js";
+
 export const renderD3 = (data, width, height) => {
 
     const{ monthlyVariance } = data;
 
     const w = width;
     const h = height;
-    const padding = 40;
+    const padding = 60;
 
     d3.select('#app')
         .append('h1')
-        .text('Monthly Global Land-Surface Temperature');
+        .text('Monthly Global Land-Surface Temperature')
+        .attr('id', 'title');
 
     const container = d3.select('#app')
         .append('div')
         .attr('id', 'container');
     
-    container.append('svg')
+    const svg = container.append('svg')
         .attr('width', w)
         .attr('height', h)
         .attr('class', 'svg');
@@ -27,19 +30,44 @@ export const renderD3 = (data, width, height) => {
         }
     });
 
-    //const xScale = d3.scaleTime()
-    //                 .domain([
-    //                    d3.min(monthlyVarianceDates.date.getFullYear()),
-    //                    d3.max(monthlyVarianceDates.date.getFullYear())
-    //                 ])
-    //                 .range([
-    //                    padding,
-    //                    (w - padding)
-    //                 ]);
-    //
-    //const yScale = d3.scaleTime()
-    //                 .domain()
+    const xScale = d3.scaleTime()
+                     .domain([
+                        d3.min(monthlyVarianceDates, d => d.date),
+                        d3.max(monthlyVarianceDates, d => d.date)
+                     ])
+                     .range([
+                        padding,
+                        (w - padding)
+                     ]);
+    
+    const yScale = d3.scaleLinear()
+                     .domain([
+                        d3.min(monthlyVarianceDates, d => d.date.getMonth()),
+                        d3.max(monthlyVarianceDates, d => d.date.getMonth())
+                     ])
+                     .range([
+                        (h - padding),
+                        padding
+                     ]);
+    
+    const xAxis = d3.axisBottom(xScale)
+        .ticks(27)
+        .tickFormat(d => d.getFullYear());
 
-    console.log(monthlyVarianceDates)
+    const yAxis = d3.axisLeft(yScale)
+        .tickFormat(d => monthsSwitch(d));
+
+    svg.append("g")
+        .attr('id', 'x-axis')
+        .attr("transform", "translate(0," + (h - padding) + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr('id', 'y-axis')
+        .attr("transform", "translate(" + padding + ", 0)")
+        .call(yAxis);
+
+    console.log(d3.min(monthlyVarianceDates, d => d.date.getFullYear()))
+    console.log(d3.max(monthlyVarianceDates, d => d.date.getFullYear()))
 
 };
