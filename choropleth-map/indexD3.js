599c92bc42072f6e4dee66ad28d26c87b669207e
Leandro_SmS <leandrosimoesms@gmail.com>
2023-06-19 07:21:18 -0300

    Set legends and colors

--- choropleth-map/indexD3.js
+++ choropleth-map/indexD3.js
@@ -1,3 +1,5 @@
+import { educationColors } from "./resources/colors.js";
+
 export const renderD3 = (data, width, height) => {
 
     const [educationData, countyData] = data;
@@ -14,6 +16,8 @@ export const renderD3 = (data, width, height) => {
 
     const geoJsonCountyData = topojson.feature(countyData, countyData.objects.counties);
 
+    const geoGenerator = d3.geoPath();
+
     const counties = svg
         .append('g')
         .attr('class', 'counties');
@@ -21,17 +25,56 @@ export const renderD3 = (data, width, height) => {
     const county = counties
         .selectAll('path')
         .data(geoJsonCountyData.features)
-        .enter()
-        .append('path')
-        .attr('d', d3.geoPath())
+        .join('path')
+        .attr('d', geoGenerator)
         .attr('class', 'county')
         .attr('data-fips', d => d.id)
         .attr('data-education', d => educationData.find(obj => obj.fips === d.id).bachelorsOrHigher);
 
-    const tooltip = d3.select('#tooltip')
+    const legendPadding = width * 0.3;
+
+    const xScaleLegend = d3.scaleLinear()
+        .domain([
+            d3.min(educationData, d => d.bachelorsOrHigher),
+            d3.max(educationData, d => d.bachelorsOrHigher)
+        ])
+        .range([
+            legendPadding,
+           (w - legendPadding)
+        ]);
+
+    const xAxisLegend = d3.axisBottom(xScaleLegend)
+        .ticks()
+        .tickFormat(d => `${d}%`);
+        
+    const legend = svg.append('svg')
+        .attr('id', 'legend');  
+
+    legend.append("g")
+        .attr('id', 'x-axis')
+        .attr('id', 'legend')
+        .attr("transform", "translate(0, 3)")
+        .call(xAxisLegend);
+
+    const ScaleColor = d3.scaleLinear()
+        .domain([
+            d3.min(educationData, d => d.bachelorsOrHigher),
+            d3.max(educationData, d => d.bachelorsOrHigher)
+        ])
+        .range([
+            0,
+            educationColors.length - 1
+        ]);
+
+        const tooltip = d3.select('#tooltip')
         .style('visibility', 'hidden');
 
     county
+        .attr('fill', d => {
+            const eduData = educationData.find(obj => obj.fips === d.id);
+            const index = Math.round(ScaleColor(eduData.bachelorsOrHigher));
+            return educationColors[index];
+        })
         .on('mouseenter', (e, d) => {
             const [x, y] = d3.pointer(e);
             const showData = educationData.find(obj => obj.fips === d.id);
@@ -39,19 +82,45 @@ export const renderD3 = (data, width, height) => {
                 .style('visibility', 'visible')
                 .style('left', (x + 20) + 'px')
                 .style('top', (y + 10) + 'px')
+                .attr('data-education', showData.bachelorsOrHigher)
                 .html(`
                     <h3>${showData.state}</h3>
                     <p>${showData.area_name}</p>
                     <p>${showData.bachelorsOrHigher}%</p>
                 `)
-            d3.select(e.target).style('fill', 'red');
+            d3.select(e.target).style('fill', '#8A2BE2');
         })
         .on('mouseleave', (e, d) => {
-            d3.select(e.target).style('fill', 'black');
+            const showData = educationData.find(obj => obj.fips === d.id);
+            const index = Math.round(ScaleColor(showData.bachelorsOrHigher));
+            d3.select(e.target)
+                .style('fill', `${educationColors[index]}`);
             tooltip.style('visibility', 'hidden');
-        })
+        });
 
-    console.log(educationData)
-    console.log(geoJsonCountyData.features)
+    const xScaleColorAxis = d3.scaleLinear()
+        .domain([
+            0,
+            educationColors.length - 1
+        ])
+        .range([
+            d3.min(educationData, d => d.bachelorsOrHigher),
+            d3.max(educationData, d => d.bachelorsOrHigher)
+        ]);
+
+    const colorWidth = xScaleLegend(xScaleColorAxis(1)) - xScaleLegend(xScaleColorAxis(0));
+
+    legend
+        .selectAll('rect')
+        .data(educationColors)
+        .join('rect')
+        .attr('x', (d, i) => xScaleLegend(xScaleColorAxis(i)))
+        .attr('y', '2px')
+        .attr('width', colorWidth)
+        .attr('height', '7px')
+        .attr('class', 'color')
+        .attr('fill', (d, i) => d);
+
+    console.log(Math.round(ScaleColor(15)))
 
 };
---
+++ choropleth-map/resources/colors.js
@@ -0,0 +1,10 @@
+export const educationColors = [
+    "#F0F8FF",    // Less education (Alice Blue)
+    "#ADD8E6",    // Light Blue
+    "#87CEEB",    // Sky Blue
+    "#6495ED",    // Cornflower Blue
+    "#4169E1",    // Royal Blue
+    "#0000FF",    // Blue
+    "#00008B",    // More education (Dark Blue)
+    "#8A2BE2"     // Highest education (Blue Violet)
+  ];
