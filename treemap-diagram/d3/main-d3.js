a94450741fccf5c0d9c285eb0ee5009c0add44f5
Leandro_SmS <leandrosimoesms@gmail.com>
2023-06-23 16:00:48 -0300

    Turn d3 into one file to render differents parameters

--- treemap-diagram/d3/main-d3.js
+++ treemap-diagram/d3/main-d3.js
@@ -1,24 +1,81 @@
-import { movieD3 } from "./movies.js";
-import { videoD3 } from "./videos.js";
-import { kickstarterD3 } from "./kickstarters.js";
+import { colors } from "../resources/colors.js";
 
 export const renderD3 = (data, id, width, height) => {
 
-    const dataId = data.name.split(' ')[0].toLowerCase();
+     //Creating hierarchy and leaves
+     const hierarchy = d3.hierarchy(data, node => node.children)
+     .sum(node => node['value'])
+     .sort((node1, node2) => node2['value'] - node1['value']);
 
-    switch (dataId) {
-        case 'video':
-            videoD3(data, id, width, height)
-            break;
-        case 'movies':
-            movieD3(data, id, width, height)
-            break;     
-        case 'kickstarter':
-            kickstarterD3(data, id, width, height)
-            break;    
-        default:
-            console.log('Error: Not a valid Id')
-            break;
-    }
+ const treemapLayout = d3.treemap()
+     .size([width, height])
+     .paddingInner(2);
+
+ treemapLayout(hierarchy);
+
+ const tilesData = hierarchy.leaves();
+
+
+ //define categories to select propr color
+ const allCategories = tilesData.map(d => d['data']['category']);
+
+ const categoriesNoRepeated = [...new Set(allCategories)];
+
+
+ //Creating visualization
+ const container = d3.select(`#${id}`);
+
+ const svg = container.append('svg')
+     .attr('width', width)
+     .attr('height', height)
+     .attr('class', 'svg ');
+
+ const tooltip = d3.select('#tooltip')
+     .style('visibility', 'hidden');
+ 
+ const block = svg.selectAll('g')
+     .data(tilesData)
+     .join('g')
+     .attr('transform', d => 'translate (' + d['x0'] + ', ' + d['y0'] +')')
+
+ const tile = block.append('rect')
+     .attr('class', 'tile')
+     .attr('fill', d => {
+         const index = categoriesNoRepeated.indexOf(d['data']['category']);
+         return colors[index];
+     })
+     .attr('width', d => d['x1'] - d['x0'])
+     .attr('height', d => d['y1'] - d['y0'])
+     .attr('data-name', d => d.data.name)
+     .attr('data-category', d => d.data.category)
+     .attr('data-value', d => d.data.value)
+     .on('mousemove', (e, d) => {
+         const {pageX, pageY} = e;
+         tooltip
+             .style('visibility', 'visible')
+             .style('left', pageX + 30 + 'px')
+             .style('top', pageY - 120 + 'px')
+             .attr('data-value', d.value)
+             .html(`
+                 <h3>${d.data.name}</h3>
+                 <p>${d.data.category}</p>
+                 <p>${d.value}%</p>
+             `)
+         d3.select(e.target).style('opacity', 0.7);
+     })
+     .on('mouseleave', (e, d) => {
+         d3.select(e.target).style('opacity', 1);
+         tooltip.style('visibility', 'hidden');
+     });
+
+ block
+     .append('text')
+     .attr('class', 'tile-text')
+     .selectAll('tspan')
+     .data(d => d.data.name.split(/(?=[A-Z][^A-Z])/g))
+     .join('tspan')
+     .attr('x', 4)
+     .attr('y', (d, i) => 13 + i * 10)
+     .text(d => d);
 
 }
